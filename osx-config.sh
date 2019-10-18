13f09fdd85e5ea72cd64cbaec1dfef792f4787f5
t-poyo <next.takesu@gmail.com>
2019-10-18 13:16:12 +0900

    add comment and trackpad speed option

--- osx-config.sh
+++ osx-config.sh
@@ -7,29 +7,43 @@ chflags nohidden ~/Library
 sudo chflags nohidden /Volumes
 
 ### Keyboard
+
+# enable fullkeyboad access
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+# input speed up
 defaults write NSGlobalDomain KeyRepeat -int 2
+# shorten time to repat same key
 defaults write NSGlobalDomain InitialKeyRepeat -int 12
 
 ### Trackpad
+
+# enable `Tap to click`
 defaults write -g com.apple.mouse.tapBehavior -int 1
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
 defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+# siraberu function
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerTapGesture -int 2
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+# speed of cursor
+defaults write -g com.apple.trackpad.scaling -float 3.0
+# TODO set click strength 'Light'
 
 ### General
+
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 defaults write com.apple.finder AppleShowAllFiles YES
 defaults write NSGlobalDomain com.apple.springing.delay -float 0
 defaults write NSGlobalDomain com.apple.springing.enabled -bool true
 defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
+# disable file-opening-aimation
 defaults write NSAutomaticWindowAnimationsEnabled -bool false
 defaults write NSWindowResizeTime -float 0.005
 defaults write com.apple.CrashReporter DialogType -string "none"
+# dont make. DS_Store
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
-defaults write com.apple.LaunchServices LSQuarantine -bool false
+# disable alert when opening DLed file
+# defaults write com.apple.LaunchServices LSQuarantine -bool false
 defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false
 
 ### Disable functions
